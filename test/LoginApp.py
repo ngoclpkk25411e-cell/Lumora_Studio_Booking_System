b5f32657724f66d91dcee18a56c81c11e0847a1d
ngoclpkk25411e-cell <ngoclpkk25411e@st.uel.edu.vn>
2026-03-25 16:44:41 +0700

    Delete test directory

--- test/LoginApp.py
+++
@@ -1,8 +0,0 @@
-import sys
-from PyQt6.QtWidgets import QApplication
-from ui.LoginMainWindowEx import LoginMainWindowEx
-
-app = QApplication(sys.argv)
-window = LoginMainWindowEx()
-window.show()
-sys.exit(app.exec())
