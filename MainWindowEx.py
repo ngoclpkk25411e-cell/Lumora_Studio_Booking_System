c52fb63de367be2d2b9b470d27af7fbc5f2454ea
nguyentnhk25411e-droid <nguyentnhk25411e@st.uel.edu.vn>
2026-03-25 14:12:46 +0700

    Add files via upload

---
+++ MainWindowEx.py
@@ -0,0 +1,11 @@
+from MainWindow import Ui_MainWindow
+
+
+
+
+class MainWindowEx(Ui_MainWindow):
+   def setupUi(self, MainWindow):
+       super().setupUi(MainWindow)
+       self.MainWindow=MainWindow
+   def showWindow(self):
+       self.MainWindow.show()
